f81d4ed8dfc9c0dbc5aa76f0d2ce2797cc715bea
xav <xavierartot@gmail.com>
2018-09-17 15:57:37 -0500

    fix: scrolling with FlatList

--- components/Decks.js
+++ components/Decks.js
@@ -16,6 +16,39 @@ import Deck from './Deck'
 import { purple, white, lightPurp } from '../utils/colors'
 import { Ionicons } from '@expo/vector-icons'
 
+class Decks extends Component {
+  state = {
+    ready: null,
+  }
+  componentDidMount() {
+    const { dispatch } = this.props
+    dispatch(handleInitDecks())
+  }
+  render() {
+    const { decks } = this.props
+    return (
+      <View style={styles.container}>
+        <View style={styles.containerTitle}>
+          <Text style={styles.title}>
+            CARD DECKS
+          </Text>
+          <Ionicons
+            color={white}
+            name="ios-arrow-round-down"
+            size={30}
+            style={{ fontSize: 25 }}
+          />
+        </View>
+        <FlatList
+          data={decks}
+          keyExtractor={(item, i) => item}
+          renderItem={({ item }) => <Deck deck={item} />}
+          style={styles.containerDecks}
+        />
+      </View>
+    )
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -46,69 +79,13 @@ const styles = StyleSheet.create({
     fontSize: 15,
   },
 })
-class Decks extends Component {
-  state = {
-    ready: null,
-  }
-  componentDidMount() {
-    const { dispatch } = this.props
-    dispatch(handleInitDecks())
-  }
-  render() {
-    const { decks } = this.props
-    return (
-      <View style={styles.container}>
-        <View style={styles.containerTitle}>
-          <Text style={styles.title}>
-            CARD DECKS
-          </Text>
-          <Ionicons
-            color={white}
-            name="ios-arrow-round-down"
-            size={30}
-            style={{ fontSize: 25 }}
-          />
-        </View>
-        <View style={styles.containerDecks}>
-          <FlatList
-            data={decks}
-            keyExtractor={(item, index) => item[item]}
-            renderItem={({ item }) => <Text>{item}</Text>}
-          />
-          {/*
-            <FlatList
-              data={['a', 'b']}
-              keyExtractor={(item, index) => item}
-              renderItem={({ item }) => <Text>{item.reply}</Text>}
-            />
-
-            <FlatList
-              data={decks}
-              keyExtractor={(deck) => deck}
-              renderItem={{deck} => (
-                <Deck deck={deck} />
-              )}
-            />
-           */}
-        </View>
-      </View>
-    )
-  }
-}
 function mapStateToProps(state, props) {
   // alert(JSON.stringify(state))
   const keys = Object.keys(state)
   const values = Object.values(state)
+  const objKeys = Object.assign({}, keys)
   // const objKeys = Object.assign({}, ['a', 'b', 'c'])// {0:"a", 1:"b", 2:"c"}
-  const objKeys = Object.assign({}, keys)// {0:"a", 1:"b", 2:"c"}
   // alert(JSON.stringify(objKeys))
-  // keys.reduce(function(result, item, index, array) {
-  // result[index] = item; //a, b, c
-  // return result;
-  // }, {}) //watch out the empty {}, which is passed as "result"
-
-  // value.map(e => alert({ ...e }))
-  // alert(keys)
   return {
     decks: Object.values(objKeys),
   }
