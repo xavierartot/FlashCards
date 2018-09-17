e87028e798a35333a231642f561ceb6b27a3a091
xav <xavierartot@gmail.com>
2018-09-17 15:52:10 -0500

    bugs: FlatList with an array-ish

--- components/Decks.js
+++ components/Decks.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { FlatList, View, Text, AsyncStorage, StyleSheet } from 'react-native'
+import {
+  ScrollView,
+  FlatList,
+  View,
+  Text,
+  AsyncStorage,
+  StyleSheet,
+} from 'react-native'
 import DeckList from './DeckList'
-import { receiveDecks } from '../actions'
-import { getDatas } from '../utils/api'
-import { handleInitDecks, LOAD_DATAS, loadDatas } from '../utils/helpers'
+// import { receiveDecks } from '../actions'
+// import { getDatas } from '../utils/api'
+import { handleInitDecks } from '../utils/helpers'
 import Deck from './Deck'
 import { purple, white, lightPurp } from '../utils/colors'
 import { Ionicons } from '@expo/vector-icons'
@@ -64,26 +71,46 @@ class Decks extends Component {
         </View>
         <View style={styles.containerDecks}>
           <FlatList
-            data={[{ key: 'a' }, { key: 'b' }]}
-            renderItem={key => <Text>{key}</Text>}
+            data={decks}
+            keyExtractor={(item, index) => item[item]}
+            renderItem={({ item }) => <Text>{item}</Text>}
           />
-          /* {
+          {/*
+            <FlatList
+              data={['a', 'b']}
+              keyExtractor={(item, index) => item}
+              renderItem={({ item }) => <Text>{item.reply}</Text>}
+            />
+
             <FlatList
               data={decks}
-              keyExtractor={(item, index) => index}
-              renderItem={deck => <Deck key={deck} deck={deck} />}
+              keyExtractor={(deck) => deck}
+              renderItem={{deck} => (
+                <Deck deck={deck} />
+              )}
             />
-          } */
+           */}
         </View>
       </View>
     )
   }
 }
 function mapStateToProps(state, props) {
+  // alert(JSON.stringify(state))
   const keys = Object.keys(state)
+  const values = Object.values(state)
+  // const objKeys = Object.assign({}, ['a', 'b', 'c'])// {0:"a", 1:"b", 2:"c"}
+  const objKeys = Object.assign({}, keys)// {0:"a", 1:"b", 2:"c"}
+  // alert(JSON.stringify(objKeys))
+  // keys.reduce(function(result, item, index, array) {
+  // result[index] = item; //a, b, c
+  // return result;
+  // }, {}) //watch out the empty {}, which is passed as "result"
+
+  // value.map(e => alert({ ...e }))
   // alert(keys)
   return {
-    decks: keys,
+    decks: Object.values(objKeys),
   }
 }
 export default connect(mapStateToProps)(Decks)
